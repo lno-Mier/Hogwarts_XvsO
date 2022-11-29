caad29527cf788196340f2781adf5a29171ea85a
lno-Mier <108462758+lno-Mier@users.noreply.github.com>
2022-11-29 18:18:47 +0600

    Update Hogwarts.js

--- Hogwarts.js
+++ Hogwarts.js
@@ -1,7 +1,27 @@
 let button = document.querySelectorAll(".buttons")
+//функция рандомного числа(активно используется)
+function Bot(max) {
+  return Math.floor(Math.random()*max)
+  if (this.classList.contains("buttonsO")) {
+  return
+  }
+  //проверка наличие buttonsO
+  if (this.classList.contains("buttonsX")) {
+  return
+  }
+}
 for (let buttonc of button) {
   buttonc.onclick = function(){
+    //проверка наличие buttonsX
+    if (this.classList.contains("buttonsO")) {
+      return
+    }
+    //проверка наличие buttonsO
+    if (this.classList.contains("buttonsX")) {
+      return
+    }
     this.classList.add("buttonsX")
-    console.log(this)
+    //сует туда непосредственно
+    button[Bot(8)].classList.add('buttonsO')
   }
 }
